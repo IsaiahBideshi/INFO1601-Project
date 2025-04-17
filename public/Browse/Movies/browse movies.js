760f955e6997578b3e3262adfa7cf13a7d42e4d4
IsaiahBideshi <isaiahbideshi@gmail.com>
2025-04-16 23:56:46 -0400

    Added account login save on all pages.

--- public/Browse/Movies/browse movies.js
+++ public/Browse/Movies/browse movies.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 });
 
-function redirectToMovie(movieID) {
+window.redirectToMovie = function redirectToMovie(movieID) {
     window.location.href = `../../Movie/Movie.html?MovieID=${movieID}`;
 }
 
@@ -118,3 +118,42 @@ function prevPage() {
     }
 }
 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import firebaseConfig from "/public/firebaseConfig.js";
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+window.showAcc = function showAcc(){
+    let accountArea = document.querySelector(".dropdown-content");
+    accountArea.style.display = "block";
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is logged in
+        console.log("User is logged in:", user);
+        console.log("User ID:", user.uid);
+        console.log("User Email:", user.email);
+        let accountArea = document.getElementById("account-area");
+        accountArea.innerHTML = `
+                    <div class="dropdown">
+                        <img style="width: 1.5em; min-height: 1.5em" src="../../assets/account-icon.png" alt="Account Icon">
+                        <div class="dropdown-content">
+                            <a href="#">Account</a>
+                            <a href="#">Log Out</a>
+                        </div>
+                    </div>
+        `;
+
+        // Access user-specific data or allow access to the page
+    } else {
+        // No user is logged in
+        console.log("No user is logged in. Redirecting to login page...");
+        window.location.href = "../Log%20In/login.html"; // Redirect to login page
+    }
+});
--- public/Movie/movie.js
+++ public/Movie/movie.js
@@ -11,7 +11,7 @@ const options = {
     }
 };
 
-async function addReview(){
+window.addReview = async function addReview(){
     console.log("Adding review " + movieID);
 
     let movieData = await fetch(`https://api.themoviedb.org/3/movie/${movieID}?language=en-US`, options);
@@ -149,3 +149,44 @@ async function getMovieDetails() {
 }
 
 getMovieDetails();
+
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import firebaseConfig from "/public/firebaseConfig.js";
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+window.showAcc = function showAcc(){
+    let accountArea = document.querySelector(".dropdown-content");
+    accountArea.style.display = "block";
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is logged in
+        console.log("User is logged in:", user);
+        console.log("User ID:", user.uid);
+        console.log("User Email:", user.email);
+        let accountArea = document.getElementById("account-area");
+        accountArea.innerHTML = `
+                    <div class="dropdown">
+                        <img style="width: 1.5em; min-height: 1.5em" src="../assets/account-icon.png" alt="Account Icon">
+                        <div class="dropdown-content">
+                            <a href="#">Account</a>
+                            <a href="#">Log Out</a>
+                        </div>
+                    </div>
+        `;
+
+        // Access user-specific data or allow access to the page
+    } else {
+        // No user is logged in
+        console.log("No user is logged in. Redirecting to login page...");
+        window.location.href = "../Log%20In/login.html"; // Redirect to login page
+    }
+});
